2479bcec2745dcfdd2b483a9ec7b651b14ea9a0b
vsmrnw <vsevolod.smrnv@gmail.com>
2024-02-08 16:28:56 +0300

    add password managment views & forms

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
-    # Login & logout views
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
 
 ]
